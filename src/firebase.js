889de513ab4c26ccf9c8babdabf64ba57ff6f0e4
Sc0tticus <ssinger303@gmail.com>
2021-05-14 17:19:50 -0600

    CH5 6s. Firebase hosting/storage setup

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+import 'firebase/storage';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyBoG33dURutzxMookJ3bzrDWkwxvnlPr3Y',
+	authDomain: 're4ct-sl4ck.firebaseapp.com',
+	projectId: 're4ct-sl4ck',
+	storageBucket: 're4ct-sl4ck.appspot.com',
+	messagingSenderId: '351043500800',
+	appId: '1:351043500800:web:a6e8427562d569635a11e5',
+	measurementId: 'G-XDN1G4YLJP'
+};
+firebase.initializeApp(config);
+
+export default firebase;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Register from './components/Auth/Register';
 import Login from './components/Auth/Login';
